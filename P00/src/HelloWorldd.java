917a93843e3bd8f54c64a2b67d5c8b44c5284618
22013795 <22013795@DESKTOP-TTPCCQ5>
2023-05-26 14:53:05 +0800

    test

---
+++ P00/src/HelloWorldd.java
@@ -0,0 +1,20 @@
+
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * 22013795, 26 May 2023 9:23:32 am
+ */
+
+public class HelloWorldd {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My First Java Program");
+	}
+
+}
